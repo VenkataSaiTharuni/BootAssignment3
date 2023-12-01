45b18d511ba3d6dee5defb2aa247870a4efe0431
VenkataSaiTharuni <151398990+VenkataSaiTharuni@users.noreply.github.com>
2023-12-01 11:41:49 +0530

    Update Cart.java

--- Downloads/SpringBootAssignment3/src/main/java/Entity/Cart.java
+++ Downloads/SpringBootAssignment3/src/main/java/Entity/Cart.java
@@ -1,67 +1,43 @@
-package Entity;
-
-import java.util.HashSet;
-import java.util.Set;
-
+package com.example.ecommerce;
+ 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-
+import java.util.List;
+ 
 @Entity
 public class Cart {
-	
-	    @Id
-
-	    @GeneratedValue(strategy = GenerationType.IDENTITY)
-
-	    private Long id;
-
-	 
-
-	    @ManyToMany(mappedBy="Carts")
-
-	    private Set<Product> products= new HashSet<>();
-
-
-
-		public Cart() {
-			super();
-		}
-
-
-
-		public Long getId() {
-			return id;
-		}
-
-
-
-		public void setId(Long id) {
-			this.id = id;
-		}
-
-
-
-		public Set<Product> getProducts() {
-			return products;
-		}
-
-
-
-		public void setProducts(Set<Product> products) {
-			this.products = products;
-		}
-
-
-
-		public Cart(Long id, Set<Product> products) {
-			super();
-			this.id = id;
-			this.products = products;
-		}
-
-}
-
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+ 
+    @ManyToMany
+    private List<Product> products;
+ 
+    // Constructors
+ 
+    public Cart() {
+        // Default constructor
+    }
+ 
+    // Getters and Setters
+ 
+    public Long getId() {
+        return id;
+    }
+ 
+    public void setId(Long id) {
+        this.id = id;
+    }
+ 
+    public List<Product> getProducts() {
+        return products;
+    }
+ 
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+ 
+}   
